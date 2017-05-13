a9450046f5d021c919d6147aacfa1b59797b8a77
Wang Jian <337034664@qq.com>
2017-05-14 01:28:34 +0800

    fix bug

--- assets/include.php
+++ assets/include.php
@@ -5,12 +5,5 @@ $ini = parse_ini_file('article.ini', TRUE);
 $path = array(
     'header' => __DIR__.'/header.php',
     'footer' => __DIR__.'/footer.php',
-
-    'about' => dirname(__DIR__).'/icons/About.svg',
-    'blog' => dirname(__DIR__).'/icons/blog.svg',
-    'home' => dirname(__DIR__).'/icons/Home.svg',
-    
-    'github' => dirname(__DIR__).'/icons/github-icon.svg',
-    'twitter' => dirname(__DIR__).'/icons/twitter-icon.svg',
 );
 ?>
