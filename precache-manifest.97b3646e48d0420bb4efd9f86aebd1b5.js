ca6c77272cec6c6a14ca0be450e1d37c9a8b4f0c
Wang Jian <337034664@qq.com>
2018-11-19 19:30:42 +0800

    Updates

---
+++ precache-manifest.97b3646e48d0420bb4efd9f86aebd1b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed8f1f9248abe94c22e5",
+    "url": "/static/js/main.ed8f1f92.chunk.js"
+  },
+  {
+    "revision": "47236916dff5e19b5d6b",
+    "url": "/static/js/1.47236916.chunk.js"
+  },
+  {
+    "revision": "ed8f1f9248abe94c22e5",
+    "url": "/static/css/main.9de0b879.chunk.css"
+  },
+  {
+    "revision": "47236916dff5e19b5d6b",
+    "url": "/static/css/1.150c20ce.chunk.css"
+  },
+  {
+    "revision": "22bb65903f8d101ab386d932fdafdb73",
+    "url": "/index.html"
+  }
+];
