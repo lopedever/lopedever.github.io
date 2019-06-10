99686b8a9fd95b1932e8e989c4a520ac9699b4f3
王建 <337034664@qq.com>
2019-06-10 22:10:51 +0800

    Updates

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # pages.wangjian.io
 
-一些简单的页面
-
 Shortcuts
 
 - [12306 SMS to Cal](https://pages.wangjian.io/#/shortcuts)
---
+++ precache-manifest.81ec05cff01111c66d98278d93e092c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f541dcd7f2a3b064f997fd21ca908f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "407e1d495529a40e0a3e",
+    "url": "/static/css/2.a72fb8c1.chunk.css"
+  },
+  {
+    "revision": "b47bb23e82e91b05cba4",
+    "url": "/static/css/main.63f9b6dd.chunk.css"
+  },
+  {
+    "revision": "407e1d495529a40e0a3e",
+    "url": "/static/js/2.ac149650.chunk.js"
+  },
+  {
+    "revision": "b47bb23e82e91b05cba4",
+    "url": "/static/js/main.dcb5b24d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
